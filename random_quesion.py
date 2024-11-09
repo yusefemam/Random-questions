7a9bb975d27385d06d03a763ce27a6ff6d57edca
yusefemam <emamyoussef49@gmail.com>
2024-11-09 16:13:53 +0200

    Update random_quesion.py

--- random_quesion.py
+++ random_quesion.py
@@ -24,7 +24,7 @@ def main():
             print(f"Let's go, {firstname}!")
         return age
 
-    def question_get():
+    def question_get(firstname):
         num1 = random.randint(1, 10)
         num2 = random.randint(1, 10)
         operation = random.choice(["+", "-", "*", "/"])
@@ -32,26 +32,31 @@ def main():
         answer = eval(f"{num1} {operation} {num2}")
         return question, answer
 
+    welcome()  
+    firstname = get_name()  
+    age = get_age(firstname)  
+    
+
     points = 0
-    questions = [question_get() for _ in range(100)]
+    questions = [question_get(firstname) for _ in range(100)]  # Generate 100 questions
 
     for i, (question, correct_answer) in enumerate(questions, 1):
         print(f"Question {i}: {question}")
         user_answer = float(input("Enter the result: "))
 
+        
         if round(correct_answer, 1) == round(user_answer, 1):
-            print(f"Correct!")
+            print(f"Correct, Good job {firstname}!")
             points += 1
         else:
             print(
-                f"Incorrect. The correct answer was {correct_answer}, you get {points} points.")
-            return
+                f"Incorrect. The correct answer was {correct_answer}. You have {points} points, Try again {firstname}!.")
+            break  
         print(f"Points: {points}")
 
+    
     print(f"You scored {points} out of 100.")
-
-    welcome()
-    firstname = get_name()
+    firstname = question_get(firstname)  
 
 
 main()
